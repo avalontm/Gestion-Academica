fe266ac404384efc498e83a5b31581ef4371b977
Λv∂lΘи ™ <jaimeraulmendezlopez325@hotmail.com>
2023-05-21 19:23:57 -0700

    updated

--- sga/DataBase/Tables/TareaUser.cs
+++ sga/DataBase/Tables/TareaUser.cs
@@ -1,24 +1,43 @@
 ﻿using PluginSQL;
+using sga.Sources.Attributes;
 
 namespace sga.DataBase.Tables
 {
     [TableName("tareas_usuarios")]
     public class TareaUser : TableBase
     {
+        [HidenField]
         [PrimaryKey]
         public int id { set; get; }
+
+        [HidenField]
         public DateTime created_at { set; get; }
+
         public DateTime updated_at { set; get; }
+
+        [HidenField]
         public int curso_id { set; get; }
+
+        [HidenField]
         public int taller_id { set; get; }
+
+        [HidenField]
         public int tarea_id { set; get; }
+
+        [HidenField]
         public int user_id { set; get; }
+
+        public int calificacion { set; get; }
+
+        public bool calificado { set; get; }
+
+        [HidenField]
         public bool eliminado { set; get; }
 
 
         public static TareaUser Find(int tarea_id)
         {
-            return MYSQL.Query<TareaUser>($"SELECT * FROM tareas_usuarios WHERE tarea_id='{tarea_id}' LIMIT 1").FirstOrDefault();
+            return MYSQL.Query<TareaUser>($"SELECT * FROM tareas_usuarios WHERE tarea_id='{tarea_id}' AND eliminado='0' LIMIT 1").FirstOrDefault();
         }
     }
 }
